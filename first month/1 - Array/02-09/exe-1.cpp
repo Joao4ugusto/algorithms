aaf3c6efbcdb91d47477bbd53f7bcd08a27f8bc6
João <joaoaugusto311016@gmail.com>
2023-09-02 16:44:05 -0300

    02-09

---
+++ first month/1 - Array/02-09/exe-1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+
+int removeDuplicates(std::vector<int>& nums) {
+    if (nums.empty()) {
+        return 0;
+    }
+
+    int k = 1;  // Inicializa k como 1, já que o primeiro elemento é sempre exclusivo
+
+    for (int i = 1; i < nums.size(); ++i) {
+        if (nums[i] != nums[i - 1]) {
+            nums[k] = nums[i];
+            ++k;
+        }
+    }
+
+    return k;
+}
+
+int main() {
+    std::vector<int> nums = {1, 2, 3};
+    int k = removeDuplicates(nums);
+
+    std::cout << "Output: " << k << ", nums = [";
+    for (int i = 0; i < k; ++i) {
+        std::cout << nums[i];
+        if (i < k - 1) {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]" << std::endl;
+
+    return 0;
+}
